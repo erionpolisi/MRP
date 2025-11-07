5962f94622d45194fdd4c82574a043f55ea2b003
Erion Polisi <Erion.Polisi@msg-plaut.com>
2025-11-07 09:59:49 +0100

    Projektdateien hinzufügen.

---
+++ MRP/Program.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>Program class.</summary>
+internal static class Program
+{
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // entry point                                                                                                      //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>Main entry point of the application.</summary>
+    /// <param name="args">Command line arguments.</param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
