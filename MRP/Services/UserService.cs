0b3f17b02b39b530fe3140c375e18fe1bcca64df
Erion Polisi <Erion.Polisi@msg-plaut.com>
2025-11-21 12:17:42 +0100

    UserService implemented (no DB)

---
+++ MRP/Services/UserService.cs
@@ -0,0 +1,64 @@
+﻿namespace MRP.System;
+
+public static class UserService
+{
+    public static (bool ok, string message, Session? session) Register(
+        string username, string fullname, string email, string password)
+    {
+        if (string.IsNullOrWhiteSpace(username))
+            return (false, "Username is required.", null);
+
+        if (UserRepository.Exists(username)) // simulate checking in database
+            return (false, "Username already exists.", null);
+
+        var user = new User
+        {
+            UserName = username,
+            FullName = fullname,
+            EMail = email
+        };
+
+        user.SetPassword(password);
+        UserRepository.Add(user); // simulate saving to database
+
+        Session? session = Session.Create(username, password);
+
+        return (true, "User registered.", session);
+    }
+
+    public static (bool ok, string message, Session? session) Login(
+        string username, string password)
+    {
+        var user = UserRepository.Get(username);// simulate fetching from database
+
+        if (user is null)
+            return (false, "Invalid username or password.", null);
+
+        Session? session = Session.Create(username, password);
+
+        if (session is null)
+            return (false, "Invalid username or password.", null);
+
+        return (true, "Login OK.", session);
+    }
+
+}
+
+// ----------------------------------------------------------
+//         will be replaced by database access later:
+// ----------------------------------------------------------
+
+public static class UserRepository
+{
+    private static readonly Dictionary<string, User> _users = new();
+
+    public static bool Exists(string username)
+        => _users.ContainsKey(username);
+
+    public static void Add(User user)
+        => _users[user.UserName] = user;
+
+    public static User? Get(string username)
+        => _users.TryGetValue(username, out var u) ? u : null;
+}
+
