fa56ca116cbfc9a4043720476ad8a46b29508447
Erion Polisi <Erion.Polisi@msg-plaut.com>
2025-11-21 12:18:11 +0100

    MediaEntry & Rating classes implemented

---
+++ MRP/System/MediaEntry.cs
@@ -0,0 +1,46 @@
+﻿namespace MRP.System
+{
+    public class MediaEntry
+    {
+        public Guid Id { get; set; }
+
+        public User Creator { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public MediaType Type { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public List<string> Genres { get; set; } = new();
+
+        public int AgeRestriction { get; set; }
+
+        public List<Rating> Ratings { get; set; } = new();
+
+        public List<User> FavoritedBy { get; set; } = new();
+
+        public enum MediaType
+        {
+            Unknown = 0,
+            Movie,
+            Series,
+            Game
+        }
+
+        public double AverageScore
+        {
+            get
+            {
+                if (Ratings.Count == 0)
+                    return 0;
+
+                return Ratings.Average(r => r.Stars);
+            }
+        }
+
+
+    }
+}
---
+++ MRP/System/Rating.cs
@@ -0,0 +1,44 @@
+﻿namespace MRP.System;
+
+public class Rating
+{
+    public Rating(User user, int stars, string? comment = null)
+    {
+        User = user;
+        SetStars(stars);
+        SetComment(comment);
+    }
+
+    public Guid Id { get; set; }
+
+    public User User { get; set; }
+
+    public int Stars { get; private set; }
+
+    public string? Comment { get; private set; } = string.Empty;
+
+    public bool IsConfirmed { get; private set; }
+
+    public DateTime Timestamp { get; private set; } = DateTime.UtcNow;
+
+    public HashSet<string> LikedByUsernames { get; set; } = new();
+
+    public void SetStars(int stars)
+    {
+        if (stars < 1 || stars > 5)
+            throw new ArgumentException("Stars must be between 1 and 5.");
+        Stars = stars;
+        IsConfirmed = false;
+    }
+
+    public void SetComment(string? comment)
+    {
+        Comment = comment;
+        IsConfirmed = false;
+    }
+
+    public void Confirm()
+    {
+        IsConfirmed = true;
+    }
+}
